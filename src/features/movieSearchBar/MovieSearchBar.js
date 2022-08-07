fd2766a76c8da5296252041fd101f0056b00da2b
Edward Chen <hi@edch.top>
2022-08-07 16:14:41 +0800

    feat: introduce redux

--- src/features/movieSearchBar/MovieSearchBar.js
+++ src/features/movieSearchBar/MovieSearchBar.js
@@ -5,7 +5,7 @@ import { SearchOutlined } from "@ant-design/icons";
 const MovieSearchBar = () => {
   return (
     <div className="movie-search-bar">
-      <Input className="search-input" placeholder="Basic usage" />
+      <Input className="search-input" placeholder="Enter the movie name" />
       <Button type="primary" icon={<SearchOutlined />}>
         Search
       </Button>
