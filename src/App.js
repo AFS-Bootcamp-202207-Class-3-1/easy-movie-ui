d9211fba306f0d54462a1ca3781e5ba372544404
Edward Chen <hi@edch.top>
2022-08-07 09:00:21 +0800

    feat: dynamic base url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import { findUserById } from "./api/user";
 
 function App() {
+  useEffect(() => {
+    async function fetchData() {
+      await findUserById(1);
+    }
+    let res = fetchData();
+    console.log(res);
+  }, []);
+
   return (
     <div className="app">
       <h1>We Are Coffee Studio.</h1>
