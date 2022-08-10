cd03f5f8b608941640eed88b9107621fd95918cf
Edward Chen <hi@edch.top>
2022-08-10 20:13:25 +0800

    feat: add select seat panel

---
+++ src/features/selectSeatPanel/SelectSeatPanel.js
@@ -0,0 +1,87 @@
+import { Checkbox, Col, Row, Divider } from "antd";
+import SelectSeatExample from "./SelectSeatExample";
+import SelectSeatItem from "./SelectSeatItem";
+import "./SelectSeatPanel.less";
+import { useState } from "react";
+
+const SelectSeatPanel = () => {
+  const initSeats = [
+    "1",
+    "0",
+    "1",
+    "0",
+    "1",
+    "0",
+    "1",
+    "0",
+    "1",
+    "0",
+    "1",
+    "0",
+    "1",
+    "0",
+    "1",
+    "0",
+    "1",
+    "0",
+    "1",
+    "0",
+    "1",
+    "0",
+    "1",
+    "0",
+    "1",
+    "0",
+    "1",
+    "0",
+    "1",
+    "0",
+    "1",
+    "0",
+    "1",
+    "0",
+    "1",
+    "0",
+  ];
+
+  const [seats, setSeats] = useState(initSeats);
+
+  const changeSeats = (index, status) => {
+    const newSeats = [...seats];
+    newSeats[index] = status;
+    setSeats(newSeats);
+  };
+
+  const checkboxGroup = seats.map((seat, index) => {
+    return (
+      <Col span={4} key={index}>
+        <SelectSeatItem
+          index={index}
+          initStatus={seat}
+          setCurrentStatus={changeSeats}
+        />
+      </Col>
+    );
+  });
+
+  return (
+    <div className="select-seat-panel">
+      <SelectSeatExample className="select-seat-panel-example" />
+      <div className="select-seat-panel-divider">
+        <Divider />
+      </div>
+      <div className="select-seat-panel-screen">
+        <div className="select-seat-panel-screen-front"></div>
+        <div className="select-seat-panel-screen-back"></div>
+        <div className="select-seat-panel-screen-text">
+          Center of the screen
+        </div>
+      </div>
+      <Checkbox.Group className="select-seat-panel-table">
+        <Row gutter={[20, 20]}>{checkboxGroup}</Row>
+      </Checkbox.Group>
+    </div>
+  );
+};
+
+export default SelectSeatPanel;
