7e71aee2fd56ee1f2fb844e078383f6955846037
Edward Chen <hi@edch.top>
2022-08-10 14:57:27 +0800

    feat: load order detail by API

--- src/pages/PrepareOrderPage.js
+++ src/pages/PrepareOrderPage.js
@@ -3,6 +3,10 @@ import "./PrepareOrderPage.less";
 import PrepareOrderDetail from "../features/prepareOrderDetail/PrepareOrderDetail";
 import { useNavigate, useParams } from "react-router-dom";
 import PerfectScrollbar from "react-perfect-scrollbar";
+import { useEffect, useState } from "react";
+import { getOrderById } from "../api/order";
+import moment from "moment";
+import { useSelector } from "react-redux";
 
 const { Step } = Steps;
 
@@ -10,9 +14,29 @@ const PrepareOrderPage = () => {
   const { orderId } = useParams();
   const navigate = useNavigate();
 
+  const [orderDetail, setOrderDetail] = useState({});
+
   const goToAfterPayPage = () => {
     navigate(`/afterPay/${orderId}`);
   };
+
+  const userInfo = useSelector((state) => state.userInfo);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await getOrderById(orderId);
+      const { schedule, movie, theater } = data;
+      setOrderDetail({
+        key: "1",
+        schedule: moment(schedule.startTime).format("YYYY-MM-DD HH:mm"),
+        movieName: movie.name,
+        theater: theater.name,
+        seat: schedule.screenText,
+        price: schedule.price,
+      });
+    };
+    fetchData();
+  }, [orderId]);
 
   return (
     <PerfectScrollbar id="app-main-scroller-bar">
@@ -27,16 +51,16 @@ const PrepareOrderPage = () => {
             </Steps>
           </div>
           <div className="prepare-order-page-content">
-            <PrepareOrderDetail />
+            <PrepareOrderDetail orderDetail={orderDetail} />
           </div>
           <div className="prepare-order-page-remain-points">
-            Remaining points : <span>10000.00</span>
+            Remaining points : <span>{userInfo.balance}</span>
           </div>
           <div className="prepare-order-page-payment">
             <div className="prepare-order-page-payment-price">
               total:
               <span className="prepare-order-page-payment-price-yen">
-                &yen; 55
+                &yen; {orderDetail.price}
               </span>
             </div>
             <Button type="primary" onClick={goToAfterPayPage}>
