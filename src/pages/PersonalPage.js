7b1ceb4f6a2bc3753f6a03e470d39335b3fc3fdc
shirelyhou0130 <h2772489816@163.com>
2022-08-10 13:36:34 +0800

    feat: navigate to orderHistory page when my order button

--- src/pages/PersonalPage.js
+++ src/pages/PersonalPage.js
@@ -1,191 +1,202 @@
-import { useLocation } from "react-router-dom";
-import { useState, useEffect } from "react";
+import {useLocation, useNavigate} from "react-router-dom";
+import {useState, useEffect} from "react";
 import {
-  UserOutlined,
-  SnippetsOutlined,
-  UploadOutlined,
-  PhoneOutlined,
-  MailOutlined,
-  AlertOutlined,
-  CalendarOutlined,
-  AccountBookOutlined,
+    UserOutlined,
+    SnippetsOutlined,
+    UploadOutlined,
+    PhoneOutlined,
+    MailOutlined,
+    AlertOutlined,
+    CalendarOutlined,
+    AccountBookOutlined,
 } from "@ant-design/icons";
-import { Menu, Button, DatePicker, Radio } from "antd";
-import { useSelector } from "react-redux";
+import {Menu, Button, DatePicker, Radio, BackTop} from "antd";
+import {useSelector} from "react-redux";
 import moment from "moment";
-import{updateUser} from '../api/user'
-import{saveUserData} from '../features/userSlice'
+import {updateUser} from '../api/user'
+import {saveUserData} from '../features/userSlice'
 import {useDispatch} from 'react-redux'
 
 import "./PersonalPage.less";
+import PerfectScrollbar from "react-perfect-scrollbar";
 
 const PersonalPage = () => {
-  const items = [
-    {
-      label: "Personal Information",
-      key: "/personal",
-      icon: <UserOutlined />,
-    },
-    {
-      label: "My Order",
-      key: "/myOrder",
-      icon: <SnippetsOutlined />,
-    },
-  ];
 
-  const handleMenuClick = () => {};
-  const location = useLocation();
+    const navigate = useNavigate();
+    const items = [
+        {
+            label: "Personal Information",
+            key: "/personal",
+            icon: <UserOutlined/>,
+        },
+        {
+            label: "My Order",
+            key: "/myOrder",
+            icon: <SnippetsOutlined/>,
+        },
+    ];
 
-  const [current, setCurrent] = useState(location.pathname);
+    const handleMenuClick = () => {
+        navigate(`/orderHistory`);
+    };
+    const location = useLocation();
 
-  const userInfo = useSelector((state) => state.userInfo);
+    const [current, setCurrent] = useState(location.pathname);
 
-  const [gender, setGender] = useState(userInfo.gender);
-  const [birthday, setBirthday] = useState(moment(userInfo.birthday));
+    const userInfo = useSelector((state) => state.userInfo);
 
-  useEffect(() => {
-    setGender(userInfo.gender);
-    setBirthday(moment(userInfo.birthday));
-  }, [userInfo]);
+    const [gender, setGender] = useState(userInfo.gender);
+    const [birthday, setBirthday] = useState(moment(userInfo.birthday));
 
-  //   setInterval(()=>{
-  //       console.log(birthday.isSame(moment(userInfo.birthday),'day'));
-  //   },1000)
+    useEffect(() => {
+        setGender(userInfo.gender);
+        setBirthday(moment(userInfo.birthday));
+    }, [userInfo]);
 
-  useEffect(() => {
-    setCurrent(location.pathname);
-  }, [location.pathname]);
+    //   setInterval(()=>{
+    //       console.log(birthday.isSame(moment(userInfo.birthday),'day'));
+    //   },1000)
 
-  const handleDateChanged = (date, dateString) => {
-    setBirthday(date);
-  };
+    useEffect(() => {
+        setCurrent(location.pathname);
+    }, [location.pathname]);
 
-  const handleGenderChanged = (event) => {
-    setGender(event.target.value);
-  };
+    const handleDateChanged = (date, dateString) => {
+        setBirthday(date);
+    };
 
-  const dispatch = useDispatch()
+    const handleGenderChanged = (event) => {
+        setGender(event.target.value);
+    };
 
-  const handleSaveClicked = () => {
-    if (
-        gender === userInfo.gender &&
-        birthday.isSame(moment(userInfo.birthday), "day")
-    ) return
-    updateUser(userInfo.id, {gender,birthday}).then(res=>{
-        dispatch(saveUserData(res))
-        console.log(res);
-    })
-    
-  };
+    const dispatch = useDispatch()
 
-  return (
-    <div className="personal">
-      <div className="personal-box w">
-        <div className="personal-box-left">
-          <Menu
-            onClick={handleMenuClick}
-            selectedKeys={[current]}
-            mode="inline"
-            items={items}
-          />
-        </div>
-        {/* 右边个人信息 开始 */}
-        <div className="personal-box-right">
-          <div className="personal-box-right">
-            <div className="personal-box-right-info">
-              <div className="personal-box-right-info-img">
-                <img className="" src={userInfo?.avatar} alt="" />
-              </div>
-              <div className="personal-box-right-info-list">
-                <div className="personal-box-right-info-list-item head">
-                  <div className="personal-box-right-info-list-item-title">
-                    <span className="nickname">Username:</span>
-                  </div>
-                  <div className="personal-box-right-info-list-item-content">
+    const handleSaveClicked = () => {
+        if (
+            gender === userInfo.gender &&
+            birthday.isSame(moment(userInfo.birthday), "day")
+        ) return
+        updateUser(userInfo.id, {gender, birthday}).then(res => {
+            dispatch(saveUserData(res))
+            console.log(res);
+        })
+
+    };
+
+    return (
+        <PerfectScrollbar id="app-main-scroller-bar">
+            <div className="personal">
+                <div className="personal-box w">
+                    <div className="personal-box-left">
+                        <Menu
+                            onClick={handleMenuClick}
+                            selectedKeys={[current]}
+                            mode="inline"
+                            items={items}
+                        />
+                    </div>
+                    {/* 右边个人信息 开始 */}
+                    <div className="personal-box-right">
+                        <div className="personal-box-right">
+                            <div className="personal-box-right-info">
+                                <div className="personal-box-right-info-img">
+                                    <img className="" src={userInfo?.avatar} alt=""/>
+                                </div>
+                                <div className="personal-box-right-info-list">
+                                    <div className="personal-box-right-info-list-item head">
+                                        <div className="personal-box-right-info-list-item-title">
+                                            <span className="nickname">Username:</span>
+                                        </div>
+                                        <div className="personal-box-right-info-list-item-content">
                     <span className="nickname">
                       <strong>{userInfo?.username}</strong>
                     </span>
-                  </div>
+                                        </div>
+                                    </div>
+                                    <div className="personal-box-right-info-list-item">
+                                        <div className="personal-box-right-info-list-item-title">
+                                            <PhoneOutlined/>
+                                            Telephone:
+                                        </div>
+                                        <div className="personal-box-right-info-list-item-content">
+                                            {userInfo?.phoneNumber}
+                                        </div>
+                                    </div>
+                                    <div className="personal-box-right-info-list-item">
+                                        <div className="personal-box-right-info-list-item-title">
+                                            <MailOutlined/>
+                                            E-mail:
+                                        </div>
+                                        <div className="personal-box-right-info-list-item-content">
+                                            {userInfo?.email}
+                                        </div>
+                                    </div>
+                                    <div className="personal-box-right-info-list-item">
+                                        <div className="personal-box-right-info-list-item-title">
+                                            <AlertOutlined/>
+                                            Gender:
+                                        </div>
+                                        <div className="personal-box-right-info-list-item-content">
+                                            <Radio.Group onChange={handleGenderChanged} value={gender}>
+                                                <Radio value={"male"}>male</Radio>
+                                                <Radio value={"female"}>female</Radio>
+                                                <Radio value={"secrecy"}>secrecy</Radio>
+                                            </Radio.Group>
+                                        </div>
+                                    </div>
+                                    <div className="personal-box-right-info-list-item">
+                                        <div className="personal-box-right-info-list-item-title">
+                                            <CalendarOutlined/>
+                                            Birthday:
+                                        </div>
+                                        <div className="personal-box-right-info-list-item-content">
+                                            <DatePicker
+                                                allowClear={false}
+                                                value={birthday}
+                                                onChange={handleDateChanged}
+                                                disabledDate={(date) => date.isAfter(moment().format())}
+                                                showToday={false}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="personal-box-right-info-list-item">
+                                        <div className="personal-box-right-info-list-item-title">
+                                            <AccountBookOutlined/>
+                                            Account:
+                                        </div>
+                                        <div className="personal-box-right-info-list-item-content">
+                                            $1000000.00
+                                        </div>
+                                    </div>
+                                    <div className="personal-box-right-info-list-item">
+                                        <div className="personal-box-right-info-list-item-title"></div>
+                                        <div className="personal-box-right-info-list-item-content">
+                                            <Button
+                                                type="primary"
+                                                onClick={handleSaveClicked}
+                                                disabled={
+                                                    gender === userInfo.gender &&
+                                                    birthday.isSame(moment(userInfo.birthday), "day")
+                                                }
+                                            >
+                                                <UploadOutlined/>
+                                                Save
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* 右边个人信息 结束 */}
                 </div>
-                <div className="personal-box-right-info-list-item">
-                  <div className="personal-box-right-info-list-item-title">
-                    <PhoneOutlined />
-                    Telephone:
-                  </div>
-                  <div className="personal-box-right-info-list-item-content">
-                    {userInfo?.phoneNumber}
-                  </div>
-                </div>
-                <div className="personal-box-right-info-list-item">
-                  <div className="personal-box-right-info-list-item-title">
-                    <MailOutlined />
-                    E-mail:
-                  </div>
-                  <div className="personal-box-right-info-list-item-content">
-                    {userInfo?.email}
-                  </div>
-                </div>
-                <div className="personal-box-right-info-list-item">
-                  <div className="personal-box-right-info-list-item-title">
-                    <AlertOutlined />
-                    Gender:
-                  </div>
-                  <div className="personal-box-right-info-list-item-content">
-                    <Radio.Group onChange={handleGenderChanged} value={gender}>
-                      <Radio value={"male"}>male</Radio>
-                      <Radio value={"female"}>female</Radio>
-                      <Radio value={"secrecy"}>secrecy</Radio>
-                    </Radio.Group>
-                  </div>
-                </div>
-                <div className="personal-box-right-info-list-item">
-                  <div className="personal-box-right-info-list-item-title">
-                    <CalendarOutlined />
-                    Birthday:
-                  </div>
-                  <div className="personal-box-right-info-list-item-content">
-                    <DatePicker
-                      allowClear={false}
-                      value={birthday}
-                      onChange={handleDateChanged}
-                      disabledDate={(date) => date.isAfter(moment().format())}
-                      showToday={false}
-                    />
-                  </div>
-                </div>
-                <div className="personal-box-right-info-list-item">
-                  <div className="personal-box-right-info-list-item-title">
-                    <AccountBookOutlined />
-                    Account:
-                  </div>
-                  <div className="personal-box-right-info-list-item-content">
-                    $1000000.00
-                  </div>
-                </div>
-                <div className="personal-box-right-info-list-item">
-                  <div className="personal-box-right-info-list-item-title"></div>
-                  <div className="personal-box-right-info-list-item-content">
-                    <Button
-                      type="primary"
-                      onClick={handleSaveClicked}
-                      disabled={
-                        gender === userInfo.gender &&
-                        birthday.isSame(moment(userInfo.birthday), "day")
-                      }
-                    >
-                      <UploadOutlined />
-                      Save
-                    </Button>
-                  </div>
-                </div>
-              </div>
             </div>
-          </div>
-        </div>
-        {/* 右边个人信息 结束 */}
-      </div>
-    </div>
-  );
+            <BackTop
+                className="app-back-to-top"
+                target={() => document.getElementById("app-main-scroller-bar")}
+            />
+        </PerfectScrollbar>
+    );
 };
 
 export default PersonalPage;
