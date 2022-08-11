b511de216113556e222d9c577fe001da0cc90f0b
Edward Chen <hi@edch.top>
2022-08-11 13:40:26 +0800

    feat: show use level and discount

--- src/api/user.js
+++ src/api/user.js
@@ -4,12 +4,16 @@ const createUser = (user) => {
   return request.post("/users", user);
 };
 
-const updateUser = (id, user) =>{
-  return request.put('/users/'+id, user)
-}
+const updateUser = (id, user) => {
+  return request.put("/users/" + id, user);
+};
 
 const findUserById = (id) => {
   return request.get(`/users/${id}`);
 };
 
-export { createUser, findUserById,updateUser };
+const getUserLevel = (id) => {
+  return request.get(`/users/level/${id}`);
+};
+
+export { createUser, findUserById, updateUser, getUserLevel };
--- src/pages/PrepareOrderPage.js
+++ src/pages/PrepareOrderPage.js
@@ -1,4 +1,4 @@
-import { Steps, Card, Button, BackTop, message } from "antd";
+import { Steps, Card, Button, BackTop, message, Modal } from "antd";
 import "./PrepareOrderPage.less";
 import PrepareOrderDetail from "../features/prepareOrderDetail/PrepareOrderDetail";
 import { useNavigate, useParams } from "react-router-dom";
@@ -8,6 +8,7 @@ import { getOrderById } from "../api/order";
 import moment from "moment";
 import { useSelector } from "react-redux";
 import { payTheOrder } from "../api/order";
+import { getUserLevel } from "../api/user";
 
 const { Step } = Steps;
 
@@ -16,6 +17,44 @@ const PrepareOrderPage = () => {
   const navigate = useNavigate();
 
   const [orderDetail, setOrderDetail] = useState({});
+  const purchasePoint = useSelector((state) => state.purchasePoint);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    Modal.success({
+      title: "VIP Rules",
+      width: 600,
+      content: (
+        <div
+          style={{
+            marginTop: "30px",
+          }}
+        >
+          <p>
+            When user has prepay 200&lt;= <span>VIP Level</span> &lt;500, then
+            define his/her VIP level as 1;
+          </p>
+          <p>
+            When user has prepay 500&lt;= <span>VIP Level</span> &lt;800, then
+            define his/her VIP level as 2;
+          </p>
+          <p>
+            When user has prepay 800&lt;= <span>VIP Level</span> , then define
+            his/her VIP level as 3;
+          </p>
+        </div>
+      ),
+      onOk() {},
+    });
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
 
   const goToAfterPayPage = async () => {
     try {
@@ -28,11 +67,36 @@ const PrepareOrderPage = () => {
   };
 
   const userInfo = useSelector((state) => state.userInfo);
+  const [userLevel, setUserLevel] = useState({});
+  const [ticketCount, setTicketCount] = useState(0);
+
+  useEffect(() => {
+    async function fetchData() {
+      const { data } = await getUserLevel(userInfo.id);
+      setUserLevel(data);
+    }
+    if (userInfo.id) {
+      fetchData();
+    }
+  }, [userInfo.id]);
 
   useEffect(() => {
     const fetchData = async () => {
       const { data } = await getOrderById(orderId);
-      const { schedule, movie, theater } = data;
+      const { schedule, movie, theater, order } = data;
+      const seatsDetail = [];
+      const splitSeats = order.seats.split("");
+      splitSeats.forEach((seat, index) => {
+        if (seat === "1") {
+          seatsDetail.push(
+            `${Math.floor(index / 6) + 1}排${
+              index - Math.floor(index / 6) * 6 + 1
+            }座`
+          );
+        }
+      });
+      setTicketCount(splitSeats.filter((seat) => seat === "1").length);
+
       setOrderDetail({
         key: "1",
         schedule: moment(schedule.startTime).format("YYYY-MM-DD HH:mm"),
@@ -40,10 +104,14 @@ const PrepareOrderPage = () => {
         theater: theater.name,
         seat: schedule.screenText,
         price: schedule.price,
+        seatsDetail,
       });
     };
     fetchData();
   }, [orderId]);
+
+  const totalPrice = orderDetail.price * ticketCount;
+  const discountPrice = totalPrice * userLevel.discount;
 
   return (
     <PerfectScrollbar id="app-main-scroller-bar">
@@ -58,16 +126,44 @@ const PrepareOrderPage = () => {
             </Steps>
           </div>
           <div className="prepare-order-page-content">
-            <PrepareOrderDetail orderDetail={orderDetail} />
+            <PrepareOrderDetail
+              orderDetail={orderDetail}
+              ticketCount={ticketCount}
+            />
           </div>
           <div className="prepare-order-page-remain-points">
-            Remaining points : <span>{userInfo.balance}</span>
+            <div>
+              Your remaining points :{" "}
+              <span className="prepare-order-page-remain-points-under">
+                $ {purchasePoint.toFixed(2)}
+              </span>
+            </div>
+            <div>
+              Your user level :{" "}
+              <span className="prepare-order-page-remain-points-under">
+                level {userLevel.level}
+              </span>
+            </div>
+            <div>
+              Your discount percent :{" "}
+              <span className="prepare-order-page-remain-points-under">
+                {parseInt((1 - userLevel.discount) * 100)}% off
+              </span>
+            </div>
+            <div className="prepare-order-page-remain-points-rules">
+              <Button onClick={showModal} type="link">
+                Click to view more discount rules
+              </Button>
+            </div>
           </div>
           <div className="prepare-order-page-payment">
             <div className="prepare-order-page-payment-price">
-              total:
+              Total Price:
               <span className="prepare-order-page-payment-price-yen">
-                &yen; {orderDetail.price}
+                &yen; {totalPrice.toFixed(2)}
+              </span>
+              <span className="prepare-order-page-payment-price-final">
+                &yen; {discountPrice.toFixed(2)}
               </span>
             </div>
             <Button type="primary" onClick={goToAfterPayPage}>
@@ -79,6 +175,26 @@ const PrepareOrderPage = () => {
           </div>
         </Card>
       </div>
+      <Modal
+        title="VIP Rules"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        width={600}
+      >
+        <p>
+          When user has prepay 200&lt;= <span>VIP Level</span> &lt;500, then
+          define his/her VIP level as 1;
+        </p>
+        <p>
+          When user has prepay 500&lt;= <span>VIP Level</span> &lt;800, then
+          define his/her VIP level as 2;
+        </p>
+        <p>
+          When user has prepay 800&lt;= <span>VIP Level</span> , then define
+          his/her VIP level as 3;
+        </p>
+      </Modal>
       <BackTop
         className="app-back-to-top"
         target={() => document.getElementById("app-main-scroller-bar")}
